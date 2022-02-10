daab46d532b5759df08368b2ae6392c88dd27548
Chris Bell <chrisbeckertonbell@gmail.com>
2022-02-09 20:08:24 -0800

    inputs sanitized

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `)
   .then(res => {
     res.rows.forEach(user => {
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = [process.argv[2] || 'JUL02'];
+
+
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -14,7 +17,7 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
 `)
   .then(res => {
